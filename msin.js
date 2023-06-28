8f16342b6f2accb2729b7aa4a6ff51c3d7bc030f
ALIMUHAMMADBeyg <131273856+ALIMUHAMMADBeyg@users.noreply.github.com>
2023-06-28 00:49:55 -0700

    Add files via upload

---
+++ msin.js
@@ -0,0 +1,59 @@
+//log to console
+console.log('Hello world');
+console.log(123);
+console.log(true);
+var greetings = 'Hello';
+console.log(greetings)
+console.log([1,2,3,4,5,6])
+console.log({a:1, b:2,})
+console.tabe({a:1, b:2,})
+
+
+console.error('This is some error');
+console.clear();
+console.log('This is a warning');
+console.time('Hello');
+   console.log('Hello world');
+   console.log('Hello world');
+   console.log('Hello world');
+   console.log('Hello world');
+   console.log('Hello world');
+   console.log('Hello world');
+   console.timeEnd('Hello')
+/*
+multi 
+line comments
+*/
+
+
+
+//Variable 
+
+
+//var, Let, Const
+
+var nam = 'John Doe';
+console.log(nam)
+nam = 'steve smith'
+
+console.log(nam)
+
+
+//init vaR
+
+
+   var greeting;
+   console.log(greeting); 
+greeting = 'Hello';
+console.log(greeting);
+
+
+//Letters , numbers _ $
+
+//can not start with number
+
+var $name = 'John';
+
+//multi word vars
+
+
